8c3f7a3195f4c59532c9c1a6a4a3e50cd48e1592
zwm-Future <871858554@qq.com>
2022-03-19 11:03:35 +0800

    修改baseUrl

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,3 +1,5 @@
 export const baseURL = 'https://cwcwx.gdut.edu.cn/reservation/api/'
+// export const baseURL = 'https://www.rdcmy.com/reservation/api/'
 
-export const webSocketUrl = 'wss://cwcwx.gdut.edu.cn/reservation/websocket/queue'
+export const webSocketUrl = 'wss://cwcwx.gdut.edu.cn/reservation/api/queue'
+// export const webSocketUrl = 'wss://www.rdcmy.com/reservation/websocket/queue'
