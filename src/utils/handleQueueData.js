c6f809cfa831298f1c2d01803e987ea37548d6ed
‘huangwantao’ <2747964040@qqcom.com>
2022-10-03 06:01:47 +0800

    代码修改

--- src/utils/handleQueueData.js
+++ src/utils/handleQueueData.js
@@ -1,64 +1,59 @@
-// 处理仅显示排队的队列信息
+// 处理大屏的队列信息
+// 此方法进行了改写 ，如果上线使用，请测试
 export function handleQueues(arr) {
-    // console.log("LOG", arr);
-    let newList = []
-    arr.forEach((v, i) => {
+    return  arr.map(v => {
+      /* -1 表示未被叫号 , 否则为叫号的窗口号 */
         let isCalled = parseInt(v.status) !== -1
-        newList.push({
+        return {
             windowNum: (<div style={{ textAlign: 'center', color: isCalled ? 'red' : '#000', fontWeight: isCalled ? 'bold' : 'normal' }} > {isCalled ? v.status : '待定'}</ div>),
             name: handleName(v.user.name + ''),
+            /* 排队序号，如果是有预约的 ，显示预约号第一个 */
             key: v.appointments ?
                 v.appointments[0].reservationNumber : v.location,
-        })
+        }
     })
-    return newList
 }
 
 // 处理姓名
+// 此方法进行了改写 ，如果上线使用，请测试
 function handleName(userName) {
     userName = userName.trim()
     let Len = userName.length
 
-    if (userName.length === 2) {
+    if (Len === 2) {
         return userName.substr(0, 1) + '*'
     }
     else {
-        let middle = '', len = Len - 2
-        while (len > 0) {
-            middle += '*'
-            len--
-        }
-        return name.substr(0, 1) + middle + name.substr(-1, 1)
+        return userName.substr(0, 1) +  ('*'.repeat(Len-2)) + userName.substr(-1, 1)
     }
 }
 
-// 获取叫号信息，返回叫号数组
-export function callQueue(queue, user) {
-    let window = user.status;
-    if (window !== "总复核") {
-        window += "号"
+// 获取叫号信息，返回叫号数组 ，每个人默认叫3次
+// 此方法进行了改写 ，如果上线使用，请测试
+export function callQueue(queue, {status:windowNum,user:{name},appointments,location}) {
+    /* 窗口号进行处理 */
+    if (windowNum !== "总复核") {
+      windowNum += "号"
     }
 
-    let userName = user.user.name;
-    console.log('用户', user);
-    let number = user.appointments ?
-        user.appointments[0].reservationNumber : user.location;
+    let number = appointments ?
+    appointments[0].reservationNumber : location;
 
-    let text = `请${queue}。排队序号为,${number},${userName},到${window}窗口`
-    return [[text, text, text].join('。。')]
+    let text = `请${queue}。排队序号为,${number},${name},到${windowNum}窗口`
+    return [[text, text, text].join('。。')]  // 句号停顿久
 }
 
 // 处理业务页面的队列
+// 此方法进行了改写 ，如果上线使用，请测试
 export function handleQueueAtP(arr) {
-    let newList = []
-    arr.forEach((v, i) => {
-        newList.push({
-            id: v.user.number,  // 学号
-            name: v.user.name,
-            key: v.appointments ?
-                v.appointments[0].reservationNumber : v.location,
-            appointments: v.appointments
-        })
+   return arr.map(({user:{number,name},appointments,location}) => {
+        return {
+            id: number,  // 学号
+            name,
+            appointments,
+            key: appointments ?
+               appointments[0].reservationNumber : location,
+
+        }
     })
-    return newList
 }
